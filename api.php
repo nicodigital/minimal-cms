976a13a5142c23621b1c9b9129b7c7faeace50ac
Nicolás González <nicolook.pro@gmail.com>
2025-04-15 08:58:02 -0300

    Improve Htaccess

--- api.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Redirige todas las solicitudes a process.php
-// Esto es necesario para mantener compatibilidad con código que puede estar
-// llamando a api.php en lugar de process.php
-
-// Transferir todos los parámetros GET y POST
-require_once 'process.php';
