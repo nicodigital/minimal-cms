f8ba29796b9db69ce4ad961bea04b5356b28130d
Nicolás González <nicolook.pro@gmail.com>
2025-04-18 09:12:37 -0300

    Improve Delete Image Style

--- js/modules/media.js
+++ js/modules/media.js
@@ -844,11 +844,11 @@ export const MediaManager = {
 
           // Botones de acción
           const actionButtons = document.createElement('div')
-          actionButtons.className = 'absolute top-1 right-1 invisible group-hover:visible flex space-x-1'
+          actionButtons.className = 'delete-image-wrap'
 
           // Botón de eliminar
           const deleteButton = document.createElement('button')
-          deleteButton.className = 'delete-image bg-red-500 hover:bg-red-700 text-white rounded-full p-1'
+          deleteButton.className = 'delete-image'
           deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>'
 
           actionButtons.appendChild(deleteButton)
