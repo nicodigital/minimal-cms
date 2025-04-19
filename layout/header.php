19e5ebc6c2af23c816347b657f3086a4d8f5ff32
Nicolás González <nicolook.pro@gmail.com>
2025-04-19 12:02:58 -0300

    Imporve Edit Filename Layout

--- layout/header.php
+++ layout/header.php
@@ -3,7 +3,7 @@
   <div class="md:w-[25%] flex justify-between items-center">
     <div class="save-wrapper flex justify-between pr-5 w-full">
       <h2 class="text-xl font-semibold current-file lg:w-[75%]">Select a file</h2>
-      <button class="edit-file-name flex bg-neutral-700 justify-center items-center size-[2.5rem] rounded-full" title="Edit file name">
+      <button class="edit-file-name" title="Edit file name">
       <svg xmlns="http://www.w3.org/2000/svg" class="size-4" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75z"/></svg>
       </button>
     </div>
