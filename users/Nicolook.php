88061cd750c4eb6856e14ee1be54af802aad4d0a
Nicolás González <nicolook.pro@gmail.com>
2025-04-19 11:49:57 -0300

    Imporve User Pass 2

--- users/Nicolook.php
+++ users/Nicolook.php
@@ -2,7 +2,5 @@
 require_once __DIR__ . '/../env-loader.php';
 
 return [
-    // Reemplaza esto con el hash SHA256 de tu contraseña
-    // https://emn178.github.io/online-tools/sha256.html
     'password' => EnvLoader::get('NICOLOOK_PASS'), 
 ];
