3eddd78d066c6bed6ee253df1a3433ca5113c499
Nicolás González <nicolook.pro@gmail.com>
2025-04-19 11:48:02 -0300

    Improve User Pass System

--- users/Nicolook.php
+++ users/Nicolook.php
@@ -1,6 +1,8 @@
 <?php
+require_once __DIR__ . '/../env-loader.php';
+
 return [
     // Reemplaza esto con el hash SHA256 de tu contraseña
     // https://emn178.github.io/online-tools/sha256.html
-    'password' => '4511f0626916246e75dca22faf41873bc14354237944a28befddd5b3233a8eb9', // @Nicolook2025
+    'password' => EnvLoader::get('NICOLOOK_PASS'), 
 ];
